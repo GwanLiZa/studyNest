b933e724928e05ce4c6f0e0fee9d81b481d18998
안동연 <a56634368@gmail.com>
2024-08-20 14:53:12 +0900

    feat: connect with DB

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, Delete, Param, Body } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Book } from './entity/book.entity'
+// import { Book } from './entity/book.entity'
+import { Book } from '@prisma/client'; 
 import { BookDto } from './dto/book.dto';
 
 import { ApiTags, ApiOperation, ApiBody, ApiResponse, ApiParam } from '@nestjs/swagger';
@@ -12,7 +13,7 @@ export class AppController {
 
   @Get()
   @ApiOperation({summary:'전체 도서', description: '모든 도서에 관련된 정보를 불러옵니다.'})
-  getAll(): Book[] {
+  getAll(): Promise<Book[]> {
     return this.appService.getAll();
   }
 
@@ -20,7 +21,7 @@ export class AppController {
   @ApiOperation({summary:'도서 검색', description: '해당 ID의 도서 정보를 불러옵니다.'})
   @ApiResponse({status:201, description:'도서 정보를 불러오는데 성공하였습니다.'})
   @ApiResponse({status:404, description:'도서 정보를 불러오는데 실패하였습니다.'})
-  getOne(@Param("id") bookId:string): Book {
+  getOne(@Param("id") bookId:string): Promise<Book> {
     return this.appService.getOne(bookId);
   }
 
@@ -28,7 +29,7 @@ export class AppController {
   @ApiOperation({summary:'도서 추가', description: '도서 정보를 추가합니다.'})
   @ApiResponse({status:201, description:'도서 정보를 추가하는데 성공하였습니다.'})
   @ApiResponse({status:404, description:'도서 정보를 추가하는데 실패하였습니다.'})
-  create(@Body() bookData: BookDto){
+  create(@Body() bookData: BookDto): Promise<Book>{
     return this.appService.create(bookData);
   }
 
@@ -36,7 +37,7 @@ export class AppController {
   @ApiOperation({summary:'도서 삭제', description: '도서 정보를 삭제합니다.'})
   @ApiResponse({status:201, description:'도서 정보를 삭제하는데 성공하였습니다.'})
   @ApiResponse({status:404, description:'도서 정보를 삭제하는데 실패하였습니다.'})
-  remove(@Param("id") bookId:string) {
+  remove(@Param("id") bookId:string): Promise<boolean>{
     return this.appService.remove(bookId);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,33 +1,52 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Book } from './entity/book.entity'
+// import { Book } from './entity/book.entity'
+import { Book } from '@prisma/client';
+import { PrismaService } from './prisma/prisma.service';
 import { BookDto } from './dto/book.dto';
 
 @Injectable()
 export class AppService {
-  private books: Book[] = [];
+  constructor(private readonly prisma: PrismaService) {}
 
-  getAll(): Book[] {
-    return this.books;
+  async getAll(): Promise<Book[]> {
+    return this.prisma.book.findMany();
   }
 
-  getOne(bookId: string): Book {
-    const book = this.books.find(book => book.id === +bookId);
+  async getOne(bookId: string): Promise<Book> {
+    const book = await this.prisma.book.findUnique({
+      where: {id: +bookId}
+    });
     if(!book) {
         throw new NotFoundException(`Book with ID: ${bookId} not found.`)
     }
     return book;
   }
 
-  create(bookData: BookDto) {
-    this.books.push({
-        id: this.books.length + 1,
+  async create(bookData: BookDto): Promise<Book> {
+    const lastBook = await this.prisma.book.findFirst({
+      orderBy: {
+        id: 'desc',
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    const newId = lastBook ? lastBook.id + 1 : 1;
+
+    return this.prisma.book.create({
+      data: {
+        id: newId,
         ...bookData
-    })
+      }
+    });
   }
 
-  remove(bookId: string) : boolean {
-    this.getOne(bookId);
-    this.books = this.books.filter(book => book.id !== +bookId);
+  async remove(bookId: string) : Promise<boolean> {
+    await this.getOne(bookId);
+    await this.prisma.book.delete({
+      where: {id: +bookId},
+    });
     return true;
   }
 
